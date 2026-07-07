a11f4078cf5b1d3928559dd50387d846e2258a50
MANI <manicallofdu@gmail.com>
2026-07-07 15:18:30 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let yes = document.getElementById("yes")
 let no = document.getElementById("no")
 let tedad = 0
 
-soal.textContent = "Will you come out with me?"
+soal.textContent = "😌میتونم به یه قرار دعوتتون کنم؟"
 
 function harekat() {
     console.log(tedad)
@@ -26,15 +26,15 @@ function harekat() {
     no.style.left = x + "px"
     no.style.top = y + "px"
 
-    if (tedad >= 4) {
-        soal.textContent = "COME ON LET ME GO...."
+    if (tedad >= 2) {
+        soal.textContent = "😒اذیت نکن دیگه"
     }
-    if (tedad >= 8) {
-        soal.textContent = "YOU ARE MAKING ME ANGRY😡"
+    if (tedad >= 3) {
+        soal.textContent = "😡داری عصبیم میکنی"
     }
-    if (tedad >= 16) {
+    if (tedad >= 5) {
         no.style.display = "none"
-        soal.textContent = "I ask again, will you come out with me?😏"
+        soal.textContent = "😏دوباره میپرسم ، میای بیرون؟"
         yes.style.width = "200px"
         yes.style.marginRight = "-100px"
     }
@@ -44,7 +44,7 @@ yes.addEventListener("click", function () {
     yes.style.width = "200px"
     yes.style.marginRight = "-100px"
     no.style.display = "none"
-    soal.textContent = "GOOD CHOICE ;)"
+    soal.textContent = "(; انتخاب خوبی بود"
 
     yes.style.color = "black"
     yes.style.fontSize = "30px"
@@ -55,7 +55,7 @@ yes.addEventListener("click", function () {
     setTimeout(function () { yes.value = "1" }, 4000)
     setTimeout(function () { yes.value = "0" }, 5000)
 
-    setTimeout(function () { window.location.href = "https://web.bale.ai/chat?uid=1882000158"; }, 5500)
+    setTimeout(function () { window.location.href = "https://web.bale.ai/chat?uid=1889602121"; }, 5500)
 })
 
 no.addEventListener("mouseover", function () {
